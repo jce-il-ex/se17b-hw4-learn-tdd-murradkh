d6b7a1b0effdab6f44e7117429dbae0951a588b2
murradkh <murradkhalil@gmail.com>
2017-05-30 12:19:31 +0300

    REFACTIORING:adding some comments

--- test/test.js
+++ test/test.js
@@ -3,16 +3,16 @@ var C = require('../cash.js');  // our module
 
 describe('Cash Register', function(){
   describe('Module C', function(){
-it('getChange(210,300) should equal [50,20,20]', function(){
+it('getChange(210,300) should equal [50,20,20]', function(){//first test
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
 })
-it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){//second test
     assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
 })
-it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){//third test
     assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
 });
-    it('should have a getChange Method', function(){
+    it('should have a getChange Method', function(){//forth test
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
     })
